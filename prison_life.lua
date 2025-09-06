2e98ce43287196540bf3283d850074c7682ba644
Silver Wolf <131898898+rbxluau@users.noreply.github.com>
2025-09-07 03:04:30 +0800

    Update and rename PrisonLife.lua to prison_life.lua

--- prison_life.lua
+++ prison_life.lua
@@ -7,7 +7,7 @@ local Players = game:GetService("Players")
 local Teams = game:GetService("Teams")
 local LocalPlayer = Players.LocalPlayer
 
-local Library, Locale = loadstring(game:HttpGet("https://raw.githubusercontent.com/rbxluau/Roblox/main/Library.lua"))()
+local Library, Locale = loadstring(game:HttpGet("https://raw.githubusercontent.com/rbxluau/script-hub/main/library.lua"))()
 
 local Window = Library:Window(Locale.PrisonLife)
 
